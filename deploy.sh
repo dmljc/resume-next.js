db4fa41e1878fb02b6902d497930bf989c0cddea
zfc <17602143402@163.com>
2025-11-29 21:04:35 +0800

    feat:把服务器文件夹名称从resume，修改为 resume-next

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ pnpm build
 # 上传到服务器
 rsync -avz --exclude 'node_modules' \
   --exclude '.git' \
-  .next/ root@8.149.129.226:/var/www/resume/.next/
+  .next/ root@8.149.129.226:/var/www/resume-next/.next/
 # 重启应用
-ssh root@8.149.129.226 "cd /var/www/resume && pm2 restart resume-next"
+ssh root@8.149.129.226 "cd /var/www/resume-next && pm2 restart resume-next"
 echo "部署完成！"
