2baccb0df44d03540272a2dbad243ebe5362d51c
zfc <17602143402@163.com>
2026-03-07 18:46:58 +0800

    fix: :bug: 更新本科为统招本科

--- src/components/sections/education.data.js
+++ src/components/sections/education.data.js
@@ -3,7 +3,7 @@ export const education = [
     {
         school: { zh: '商丘工学院', en: 'Shangqiu Institute of Technology' },
         degree: {
-            zh: '软件工程（辅修） | 本科',
+            zh: '软件工程（辅修）|  统招本科',
             en: 'Bachelor’s Degree (Minor in Software Engineering)',
         },
         time: { zh: '2012-2016', en: '2012–2016' },
